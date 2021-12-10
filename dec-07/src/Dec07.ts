fbd111e1dd54c3d6449074ccdcb9a854813dbd2b
Stephen Cheng <16949280+Aw3someOne@users.noreply.github.com>
2021-12-10 02:48:57 -0800

    dec-07 part 2

--- dec-07/src/Dec07.ts
+++ dec-07/src/Dec07.ts
@@ -21,6 +21,21 @@ export class Dec07 {
   }
 
   public part2(): number {
-    throw new Error('not implemented');
+    const min = Math.min(...this.positions);
+    const max = Math.max(...this.positions);
+
+    let pos = 0;
+    let minFuel = Number.MAX_SAFE_INTEGER;
+    for (let i = min; i <= max; i++) {
+      const fuel = this.positions.reduce((p, c) => {
+        const delta = Math.abs(c - i);
+        return p + delta * (1 + delta) / 2;
+      }, 0);
+      if (fuel < minFuel) {
+        minFuel = fuel;
+        pos = i;
+      }
+    }
+    return minFuel;
   }
 }
--- dec-07/src/index.test.ts
+++ dec-07/src/index.test.ts
@@ -18,13 +18,13 @@ describe('should work', () => {
   });
 });
 
-// const p2: TestCase[] = [
-//   [[testInput], 0],
-// ];
+const p2: TestCase[] = [
+  [[testInput], 168],
+];
 
-// describe('should work', () => {
-//   it.each(p2)('should work', (params, expected) => {
-//     const engine = new Dec07(...params);
-//     expect(engine.part2()).toEqual(expected);
-//   });
-// });
+describe('should work', () => {
+  it.each(p2)('should work', (params, expected) => {
+    const engine = new Dec07(...params);
+    expect(engine.part2()).toEqual(expected);
+  });
+});
