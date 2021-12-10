b55c12c2e8c770a81d06412bef7aab20f3525adf
Stephen Cheng <16949280+Aw3someOne@users.noreply.github.com>
2021-12-10 02:43:58 -0800

    dec-07 part 1

---
+++ dec-07/src/Dec07.ts
@@ -0,0 +1,26 @@
+export class Dec07 {
+  private positions: number[];
+  public constructor(input: string[]) {
+    this.positions = input[0].split(',').map(s => +s);
+  }
+
+  public part1(): number {
+    const min = Math.min(...this.positions);
+    const max = Math.max(...this.positions);
+
+    let pos = 0;
+    let minFuel = Number.MAX_SAFE_INTEGER;
+    for (let i = min; i <= max; i++) {
+      const fuel = this.positions.reduce((p, c) => p + Math.abs(c - i), 0);
+      if (fuel < minFuel) {
+        minFuel = fuel;
+        pos = i;
+      }
+    }
+    return minFuel;
+  }
+
+  public part2(): number {
+    throw new Error('not implemented');
+  }
+}
---
+++ dec-07/src/index.test.ts
@@ -0,0 +1,30 @@
+
+import fs from 'fs';
+import { EOL } from 'os';
+import { Dec07 } from './Dec07';
+
+type TestCase = [[string[]], number];
+
+const testInput = fs.readFileSync('test').toString().split(EOL);
+
+const p1: TestCase[] = [
+  [[testInput], 37],
+];
+
+describe('should work', () => {
+  it.each(p1)('should work', (params, expected) => {
+    const engine = new Dec07(...params);
+    expect(engine.part1()).toEqual(expected);
+  });
+});
+
+// const p2: TestCase[] = [
+//   [[testInput], 0],
+// ];
+
+// describe('should work', () => {
+//   it.each(p2)('should work', (params, expected) => {
+//     const engine = new Dec07(...params);
+//     expect(engine.part2()).toEqual(expected);
+//   });
+// });
